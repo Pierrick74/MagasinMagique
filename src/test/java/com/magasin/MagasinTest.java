697b9f00c52bb7d7eef0cf95e2ae4f86a477b4f4
pierrick <pierrickviret@gmail.com>
2025-09-03 14:06:22 +0200

    feat(Test) add Master Golden with approvaltests library

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,17 +1,25 @@
 package com.magasin;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        Magasin app = new Magasin(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void testList() {
+        String[] names = new String[] {"Comté", "Kryptonite", "Pass VIP Concert", "Pouvoirs magiques", "kryptonite", "comté"};
+        Integer[] sellins = new Integer[] {8, -1, 0, 2, 4, 5, 6, 9, 10, 11};
+        Integer[] qualities = new Integer[] {1, 0, 20, 50, 49, 51, 80, };
+
+        CombinationApprovals.verifyAllCombinations(
+                (String name, Integer sellin, Integer quality) -> {
+                    Item[] items = new Item[] { new Item(name, sellin, quality) };
+                    Magasin app = new Magasin(items);
+                    app.updateQuality();
+                    return items[0].toString();
+                },
+                names, sellins, qualities
+        );
     }
 
 }
