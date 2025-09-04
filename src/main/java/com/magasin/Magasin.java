88deaa9c32b421a837354b1d562fb119c46756c3
pierrick <pierrickviret@gmail.com>
2025-09-04 10:10:35 +0200

    refact(magasin) update logic with switch

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,53 +9,58 @@ class Magasin {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Kryptonite")) {
-                break;
-            }
 
-            item.quality = calculeItemQualityUpdate(item);
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                item.quality = calculeItemQualityWhenExpirationPassed(item);
+            switch (item.name) {
+                case "Kryptonite":
+                    break;
+                case "Pass VIP Concert":
+                    updatePassViP(item);
+                    break;
+                case "Comté":
+                    updateComte(item);
+                    break;
+                default:
+                    update(item);
+                    break;
             }
         }
     }
 
-    private Integer calculeItemQualityWhenExpirationPassed(Item item) {
-        if (item.name.equals("Comté")) {
-                return (item.quality < 50) ? item.quality + 1 : item.quality;
-        }
+    private void updateComte(Item item){
 
-        if (item.name.equals("Pass VIP Concert")) {
-            return 0;
-        }
+        item.quality = item.quality < 50 ? item.quality + 1 :  item.quality;
 
-        return item.quality > 0 ? item.quality - 1 : item.quality;
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
+        }
     }
 
-    private int calculeItemQualityUpdate(Item item) {
-        if (item.name.equals("Comté")) {
-            return item.quality < 50 ? item.quality + 1 :  item.quality;
+    private void updatePassViP(Item item){
+
+        if (item.quality >= 50) {
+        } else if (item.sellIn < 6) {
+            item.quality = Math.min((item.quality + 3), 50);
+        } else if (item.sellIn < 11) {
+            item.quality = Math.min((item.quality + 2), 50);
+        } else {
+            item.quality = item.quality + 1;
         }
 
-        if (item.name.equals("Pass VIP Concert")) {
-            if (item.quality >= 50) {
-                return item.quality;
-            }
+        item.sellIn = item.sellIn - 1;
 
-            if (item.sellIn < 6) {
-                return Math.min((item.quality + 3), 50);
-            }
-
-            if (item.sellIn < 11) {
-                return Math.min((item.quality + 2), 50);
-            }
-
-            return item.quality = item.quality + 1;
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
+    }
 
-        return item.quality > 0 ? item.quality - 1 : item.quality;
+    private void update(Item item) {
+        item.quality =  item.quality > 0 ? item.quality - 1 : item.quality;
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = item.quality > 0 ? item.quality - 1 : item.quality;
+        }
     }
 }
